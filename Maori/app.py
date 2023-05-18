ef7d095a73d9fb132e60fc4ab10fae6d64226e63
jonesbbqfootmassage <133757489+jonesbbqfootmassage@users.noreply.github.com>
2023-05-18 13:04:43 +1200

    Add files via upload

--- Maori/app.py
+++ Maori/app.py
@@ -16,16 +16,39 @@ def open_database(db_file):     # function to connect to database
     return None
 
 
+def is_logged_in():     # shows if or if not logged in
+    if session.get("email") is None:
+        print("not Logged in")
+        return False
+    else:
+        print("Logged in")
+        return True
+
+
 @app.route('/')     # calls main page
 def render_homepage():
     return render_template('home.html')
 
 
-@app.route('/vocab')     # calls vocabulary page
-def render_vocab():
+@app.route('/vocab/<cat_id>')     # calls vocabulary page
+def render_vocab(cat_id):
     con = open_database(DATABASE)
-    query = "SELECT * FROM words_list WHERE cat_id = ? ORDER BY Category"
-    return render_template('vocab.html')
+    query = "SELECT * FROM category_table"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    print(category_list)
+
+    query = "SELECT * FROM words_list WHERE cat_id= ? ORDER BY ID"
+    cur = con.cursor()
+    cur.execute(query, (cat_id, ))
+    words_list = cur.fetchall()
+    print(words_list)
+    con.close()
+
+    return render_template('vocab.html', words=words_list, categories=category_list     )
+
+
 
 
 app.run(host='0.0.0.0', debug=True)
